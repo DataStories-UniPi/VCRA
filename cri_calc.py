3feaf910ae565a860efbdc85486cd45c65f3366a
Andreas Tritsarolis <tri.andre7@gmail.com>
2023-06-28 08:59:13 +0300

    Updated CRI Equations

--- cri_calc.py
+++ cri_calc.py
@@ -1,105 +1,91 @@
 import numpy as np
 import cri_helper as helper
+import py.st_toolkit as stt
 
 EPS = 1e-9
 
 
-def calculate_azimuth_angle(xr, yr, eps=EPS):
-    # Rectify Azimuth Angle
-    beta = helper.rectify_direction(xr, yr)
+def calculate_cpa(own, target):
+    """
+        Return DCPA, TCPA, etc. (use prior to CRI calculation)
+
+        Outputs
+        ==========
+        * dist_euclid: The vessels' distance (in NAUTICAL MILES)
+        * speed_r: The vessels' relative speed magnitude (in KNOTS)
+        * rel_movement_direction: The vessels' relative speed direction (in RADIANS)
+        * azimuth_angle_target_to_own: The vessels' azimuth (in RADIANS)
+        * relative_bearing_target_to_own: The vessels' relative bearing (in RADIANS)
+        * DCPA: The Distance to the Closest Point of Approach ([D]CPA; in NAUTICAL MILES)
+        * TCPA: The Time to the Closest Point of Approach ([T]CPA; in HOURS)
+    """
+
+    # Calculate D (points' distance)
+    dlon, dlat = helper.calculate_delta(own['geometry'].x, target['geometry'].x), \
+                 helper.calculate_delta(own['geometry'].y, target['geometry'].y)
+
+    dist_euclid = stt.haversine(own['geometry'], target['geometry']) / 1.852
+
+    # Calculate σ (direction of relative movement direction)
+    speed_rx, speed_ry, speed_r = helper.relative_speed(target['speed'], target['course_rad'],
+                                                        own['speed'], own['course_rad'])
+
+    rel_movement_direction = helper.azimuth(speed_rx, speed_ry)
+
+    # Calculate $TB_{OS}^{TS}$ (vessels' azimuth)
+    azimuth_angle_target_to_own = helper.azimuth(dlon, dlat)
+
+    # Calculate $TB_{OS}^{TS}$ (vessels' azimuth)
+    relative_bearing_target_to_own = azimuth_angle_target_to_own - own['course_rad']
+
+    # Calculate CPA
+    CPA_angle = rel_movement_direction - azimuth_angle_target_to_own - np.pi
+    DCPA = dist_euclid * np.sin(CPA_angle)
+    TCPA = dist_euclid * np.cos(CPA_angle) / (speed_r + EPS)
+
+    return dist_euclid, speed_rx, speed_ry, speed_r, \
+           rel_movement_direction % (2*np.pi), \
+           azimuth_angle_target_to_own  % (2*np.pi), \
+           relative_bearing_target_to_own % (2*np.pi), \
+           DCPA, TCPA
+
+
+def calculate_cri(own, target, dist_euclid, speed_r, rel_movement_direction, azimuth_angle_target_to_own,
+                  relative_bearing_target_to_own, dcpa, tcpa, stationary_speed_threshold=1, weights=[0.4457, 0.2258, 0.1408, 0.1321, 0.0556]):
+    if tcpa <= 0 or own['speed'] <= stationary_speed_threshold:
+        return np.nan, np.nan, np.nan, np.nan, np.nan, 0
+
+    d1, d2 = helper.calculate_ship_safety_domain(azimuth_angle_target_to_own)
+    U_dcpa = helper.cpa_membership(np.abs(dcpa), d1, d2)
+
+    t1, t2 = helper.calculate_collision_eta(np.abs(dcpa), speed_r + EPS, d1, d2)
+    U_tcpa = helper.cpa_membership(np.abs(tcpa), t1, t2)
+
+    crit_safe_dist, avoid_measure_dist = helper.calculate_critical_distance(own['length_nmi'], relative_bearing_target_to_own)
+    U_dist = helper.cpa_membership(dist_euclid, crit_safe_dist, avoid_measure_dist)
+
+    U_bearing = helper.bearing_membership(relative_bearing_target_to_own)
+
+    speed_ratio = target['speed'] / (own['speed'] + EPS)
+    U_speed = helper.speed_ratio_membership(speed_ratio, rel_movement_direction)
+
+    return U_dcpa, U_tcpa, U_dist, U_bearing, U_speed, np.dot(weights, [U_dcpa, U_tcpa, U_dist, U_bearing, U_speed])
+
+
+def calculate_cri_vcra(own, target, dist_euclid, rel_movement_direction, azimuth_angle_target_to_own, tcpa, 
+                       vcra_model, stationary_speed_threshold=1):
+    if tcpa <= 0 or own['speed'] <= stationary_speed_threshold:
+        return np.nan, np.nan, np.nan, np.nan, np.nan, 0
     
-    return np.arctan(xr/(yr + eps)) + beta
-
-
-def colregs_alarms(own, target, eps=EPS):
+    ''' 
+        VCRA Input: [
+            'own_speed', 'own_course_rad', 
+            'target_speed', 'target_course_rad', 
+            'dist_euclid', 'azimuth_angle_target_to_own', 'rel_movement_direction'
+        ]; Output: CRI [0, 1]
     '''
-    Return DCPA, TCPA, etc. (use prior to CRI calculation)
-    '''
-    own_geom_nm, target_geom_nm = map(helper.angular_to_nautical_miles, [own['geom'], target['geom']])
-    
-    # Get vessels' relative position (w.r.t target vessel) -- NAUTICAL MILES
-    xr, yr = helper.calculate_delta(own_geom_nm.x, target_geom_nm.x), helper.calculate_delta(own_geom_nm.y, target_geom_nm.y)
+    vcra_input = np.array(
+        [own['speed'], own['course_rad'], target['speed'], target['course_rad'], dist_euclid, azimuth_angle_target_to_own, rel_movement_direction]
+    ).reshape(1, -1)
 
-    # Get True Azimuth angle (w.r.t target vessel) -- RADIANS
-    azimuth_angle = calculate_azimuth_angle(xr, yr, eps=eps)
-
-    # Get vessels' relative Course over Ground (w.r.t target vessel) -- DEGREES
-    hr = helper.calculate_delta(own['course'], target['course'])
-
-    # Get vessels' speed components (w.r.t x- and y-axis) -- KNOTS
-    speed_tx, speed_ty = helper.calculate_speed_components(target['speed'], target['course'])
-    # speed_ox, speed_oy = helper.calculate_speed_components(own['speed'], own['course']+hr)
-    speed_ox, speed_oy = helper.calculate_speed_components(own['speed'], own['course'])
-
-    # Get vessels' relative Speed (w.r.t target vessel) -- KNOTS
-    speed_rx, speed_ry = helper.calculate_delta(speed_ox, speed_tx), helper.calculate_delta(speed_oy, speed_ty)
-    speed_r = np.sqrt(speed_rx**2 + speed_ry**2)
-
-    # Get vessels' Euclidean Distance -- METERS
-    dist_euclid = np.sqrt(xr**2 + yr**2)
-
-    # Get vessels' relative Movement angle (w.r.t target vessel) -- RADIANS
-    rel_movement_angle = calculate_azimuth_angle(speed_rx, speed_ry, eps=eps)
-
-    # Get vessels' DCPA, and TCPA -- NAUTICAL MILES and HOURS
-    ves_angle = rel_movement_angle - azimuth_angle - np.pi
-    ves_dcpa = dist_euclid * np.sin(ves_angle)
-    ves_tcpa = dist_euclid * np.cos(ves_angle) / (speed_r + EPS)
-
-
-    return ves_dcpa, ves_tcpa, hr, rel_movement_angle, dist_euclid, speed_r
-
-
-def calculate_cri(own, target, ves_dcpa, ves_tcpa, hr, rel_movement_angle, dist_euclid, speed_r, weights=[0.4457, 0.2258, 0.1408, 0.1321, 0.0556], eps=EPS):
-    speed_ratio = target['speed'] / (own['speed'] + eps)
-    # Get vessels' Speed Membership Score
-    U_speed = helper.speed_ratio_membership(speed_ratio, rel_movement_angle)
-    # U_speed = helper.speed_ratio_membership(speed_ratio, hr)
-    
-    # Get vessels' Course Membership Score
-    U_course = helper.relative_course_membership(hr)
-    # U_course = helper.relative_course_membership(rel_movement_angle)
-
-    # Get vessels' Distance Membership Score
-    d_min, d_max = helper.get_safe_domain(own['length'], hr)
-    # d_min, d_max = helper.get_safe_domain(own['length'], rel_movement_angle)
-    U_dist = helper.distance_membership(dist_euclid, d_min, d_max)
-    
-    # Get vessels' DCPA Membership Score
-    d_crit = helper.get_critical_distance(rel_movement_angle)
-    # d_crit = helper.get_critical_distance(hr)
-    d_safe = 2 * d_crit
-    U_dcpa = helper.dcpa_membership(ves_dcpa, d_crit, d_safe)
-
-    # Get vessels' TCPA Membership Score
-    t_crit, t_safe = helper.get_response_range(d_crit, d_safe, ves_dcpa, speed_r, eps=eps)
-    U_tcpa = helper.tcpa_membership(ves_tcpa, t_crit, t_safe)
-    
-    # if ves_tcpa < 0 or ves_dcpa < 0:
-    if ves_tcpa < 0:
-        return 0
-    
-    return np.dot(weights, [U_dcpa, U_tcpa, U_dist, U_course, U_speed])
-
-
-if __name__ == '__main__':
-    import shapely
-
-    own = dict(
-        geom   = shapely.geometry.Point(2.19, 39.4),
-        speed  = 30,
-        course = 80,
-        length = 143
-    )  
-
-    target = dict(
-        geom   = shapely.geometry.Point(2.7, 39.4),
-        speed  = 10,
-        course = 190,
-        length = 208
-    )
-
-    ves_dcpa, ves_tcpa, hr, rel_movement_angle, dist_euclid, speed_r = colregs_alarms(own=own, target=target)
-    ves_cri = calculate_cri(own, target, ves_dcpa, ves_tcpa, hr, rel_movement_angle, dist_euclid, speed_r)
-    
-    print(ves_cri)
+    return np.nan, np.nan, np.nan, np.nan, np.nan, max(0, min(1, vcra_model.predict(vcra_input)[0]))
--- cri_helper.py
+++ cri_helper.py
@@ -6,126 +6,143 @@ from pyproj import Proj, Transformer
 
 EPS = 1e-9
 
-
 angular_to_nautical_miles = lambda x: shapely.geometry.Point(*[np.divide(i,1852) for i in transform_geometry(x).coords.xy])
+
+degree_geo2math = lambda x: (450 - x) % 360
+
+degrees_to_radians = lambda x: np.deg2rad(x)
+
+
+def homogenize_units(vcra_data):
+    # Unit Conversions (for the sake of homogeneity)
+
+    ## Convert COURSE from DEGREES to RADIANS
+    vcra_data.insert(
+        vcra_data.columns.get_loc('own_course') + 1, 
+        'own_course_rad', 
+        vcra_data.own_course.apply(degrees_to_radians)
+    )
+
+    vcra_data.insert(
+        vcra_data.columns.get_loc('target_course') + 1, 
+        'target_course_rad', 
+        vcra_data.target_course.apply(degrees_to_radians)
+    )
+
+
+    ## Convert LENGTH from METERS to NAUTICAL MILES
+    vcra_data.insert(
+        vcra_data.columns.get_loc('own_length') + 1, 
+        'own_length_nmi', 
+        vcra_data.own_length / 1852
+    )
+
+    vcra_data.insert(
+        vcra_data.columns.get_loc('target_length') + 1, 
+        'target_length_nmi', 
+        vcra_data.target_length / 1852
+    )
+    
+    return vcra_data
 
 
 def transform_geometry(point, crs_from='epsg:4326', crs_to='epsg:3857'):
-	'''
+    '''
         Transform the CRS of a ```shapely.geometry``` instance
-	'''
-	project = Transformer.from_crs(crs_from, crs_to, always_xy=True)
-	return transform(project.transform, point)
+    '''
+    project = Transformer.from_crs(crs_from, crs_to, always_xy=True)
+    return transform(project.transform, point)
 
 
-def rectify_direction(xr, yr):
-    if xr >= 0 and yr >= 0:
-        return 0
-    
-    elif xr >= 0 and yr < 0:
-        return np.pi
-    
-    elif xr < 0 and yr >= 0:
-        return 2 * np.pi
-    
-    # All negative    
-    return np.pi
+def calculate_delta(feat_own, feat_target):
+    return feat_target - feat_own
+
+
+def azimuth(dx, dy):
+    return np.arctan2(dx, dy)
+
+
+def relative_speed(speed_ts, course_ts, speed_os, course_os):
+    speed_rx = speed_ts * np.sin(course_ts) - speed_os * np.sin(course_os)
+    speed_ry = speed_ts * np.cos(course_ts) - speed_os * np.cos(course_os)
+    return speed_rx, speed_ry, np.linalg.norm([speed_rx, speed_ry])
 
 
 def calculate_delta(first, second):
     return second - first
 
 
-def calculate_speed_components(speed, course):
-    course_rad = np.deg2rad(course)
-    
-    return (speed * np.sin(course_rad), 
-            speed * np.cos(course_rad))
+def calculate_ship_safety_domain(azimuth_angle):
+    """
+        $d_1$: Ship safety domain
+        $d_2$: Ship non-risk boundary
+
+        Input
+        =====
+          * azimuth_angle (in RADIANS)
+
+        Output
+        =====
+          * $d_1$, $d_2$
+    """
+    # ## Calculate $d_1$, and $d_2$. The latter is twice the value of the former,
+    # ## therefore, we only need to calculate $d_1$, given the vessels' azimuth angle $TB_{OS}^{TS}$
+    d1 = np.nan
+
+    angles = np.array([0, 5*np.pi/8, np.pi, 11*np.pi/8, 2*np.pi])     # FROM THE "Wang et al., 2021" PAPER
+    # angles = np.array([0., 112.5, 180., 247.5, 360.])                 # The above parameters, but in degrees.
+
+    if angles[0] <= azimuth_angle < angles[1]:
+        d1 = 1.1 - 0.2 * azimuth_angle / np.pi
+
+    elif angles[1] <= azimuth_angle < angles[2]:
+        d1 = 1.0 - 0.4 * azimuth_angle / np.pi
+
+    elif angles[2] <= azimuth_angle < angles[3]:
+        d1 = 1.0 - 0.4 * (2 * np.pi - azimuth_angle) / np.pi
+
+    elif angles[3] <= azimuth_angle < angles[4]:
+        d1 = 1.1 - 0.4 * (2 * np.pi - azimuth_angle) / np.pi
+
+    return d1, 2 * d1
 
 
-def calculate_azimuth_angle(xr, yr, eps=EPS):
-    # Rectify Azimuth Angle
-    beta = rectify_direction(xr, yr)
-    
-    return np.arctan(xr/(yr + eps)) + beta
-
-
-def speed_ratio_membership(speed_ratio, relative_course, eps=EPS):
-    # Speed Ratio membership 
-    denom  = speed_ratio * np.sqrt(speed_ratio**2 + 1 - 2 * speed_ratio * np.sin(np.deg2rad(relative_course)))
-    sdenom = 1 / (1 + 2 / (denom + eps))
-    
-    return sdenom
-
-
-def relative_course_membership(relative_bearing):
-    # Relative Course/Bearing Membership
-    x = np.deg2rad(relative_bearing - 19)
-    
-    return 1/2 * (np.cos(x) + np.sqrt(440/289 + np.cos(x)**2)) - 5/17
-
-
-def get_safe_domain(vessel_length, relative_bearing):
-    x = np.deg2rad(relative_bearing - 19)
-    
-    d_min = 12 * (vessel_length / 1852)
-    d_max = 1.7 * np.cos(x) + np.sqrt(4.4 + 2.89 * np.cos(x)**2)
-    
-    return d_min, d_max
-
-
-def normalize(val, v_min, v_max):
-    return ((v_max - val) / (v_max - v_min))**2
-    
-
-def distance_membership(distance, d_min, d_max):
-    # Distance Membership
-    if distance < d_min:
+def cpa_membership(value, range_min, range_max):
+    if value <= range_min:
         return 1
-    elif d_min <= distance <= d_max:
-        return normalize(distance, d_min, d_max)
+    elif range_min < value <= range_max:
+        return ((range_max - value) / (range_max - range_min))**2
     else:
         return 0
 
 
-def get_critical_distance(relative_bearing):
-    if np.deg2rad(67.5) < relative_bearing <= np.deg2rad(112.5):
-        return 1
-    elif np.deg2rad(112.5) < relative_bearing <= np.deg2rad(247.5):
-        return 0.6
-    elif np.deg2rad(247.5) < relative_bearing <= np.deg2rad(355):
-        return 0.9
+def calculate_collision_eta(dcpa, speed_r, d1, d2):
+    nominator = d1 ** 2 - dcpa ** 2
+
+    if dcpa <= d1:
+        t1 = np.sqrt(nominator) / speed_r
     else:
-        return 1.1
-    
-    
-def dcpa_membership(dcpa, d_crit, d_safe):
-    # DCPA Membership
-    if np.abs(dcpa) < d_crit:
-        return 1
-    elif d_crit <= np.abs(dcpa) <= d_safe:
-        return normalize(np.abs(dcpa), d_crit, d_safe)
-    else:
-        return 0
+        t1 = (d1 - dcpa) / speed_r
+
+    t2 = np.sqrt(d2 ** 2 - dcpa ** 2) / speed_r
+    return t1, t2
 
 
-def get_response_range(d_crit, d_safe, dcpa, relative_speed, eps=EPS):
-    t_crit, t_safe = 0, 0
-    
-    if np.abs(dcpa) <= d_crit:
-        t_crit = np.sqrt(d_crit**2 - dcpa**2) / (relative_speed + eps)
-    
-    if np.abs(dcpa) <= d_safe:
-        t_safe = np.sqrt(d_safe**2 - dcpa**2) / (relative_speed + eps)
-    
-    return t_crit, t_safe
-      
-    
-def tcpa_membership(tcpa, t_crit, t_safe):    
-    # TCPA Membership
-    if np.abs(tcpa) < t_crit:
-        return 1
-    elif t_crit <= np.abs(tcpa) <= t_safe:
-        return normalize(np.abs(tcpa), t_crit, t_safe)
-    else:
-        return 0
+def calculate_critical_distance(length_own, relative_bearing_target_to_own):
+    crit_safe_dist = 12 * length_own
+
+    avoid_measure_angle = relative_bearing_target_to_own - np.deg2rad(19)
+    avoid_measure_dist = 1.7 * np.cos(avoid_measure_angle) + np.sqrt(4.4 + 2.89 * np.cos(avoid_measure_angle) ** 2)
+
+    return crit_safe_dist, avoid_measure_dist
+
+
+def bearing_membership(relative_bearing_target_to_own):
+    avoid_measure_angle = relative_bearing_target_to_own - np.deg2rad(19)
+    return 1/2 * (np.cos(avoid_measure_angle) + np.sqrt(440/289 + np.cos(avoid_measure_angle)**2)) - 5/17
+
+
+def speed_ratio_membership(speed_ratio, relative_course):
+    denom = speed_ratio * np.sqrt(speed_ratio**2 + 1 + 2 * speed_ratio * np.sin(relative_course)) + EPS
+    assert denom > 0, ValueError(f'Division by zero {speed_ratio=}, {relative_course=}, {denom=}')
+    return 1/(1 + 2/denom)
